3a5650b246fbb4772951b0109279ec2afe72e9e2
Zurdge <jamesedburdge@gmail.com>
2019-11-02 02:12:13 +0900

    Animated Searchbar - Basic setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,101 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Style from './App.module.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Searchbar from './input-text.js';
+
+const worklist = ['Enter your dream location', 'Why not...Tokyo?', 'Melbourne?', 'Sydney?'];
+
+class NewComponent extends React.Component{
+  render(){
+    return (
+      <div className={Style.credit}>
+      <a style={{backgroundColor: 'black', color: 'white', textDecoration: 'none', padding: '4px 6px', fontFamily: '-apple-system, BlinkMacSystemFont, "San Francisco", "Helvetica Neue", Helvetica, Ubuntu, Roboto, Noto, "Segoe UI", Arial, sans-serif', fontSize: '12px', fontWeight: 'bold', lineHeight: '1.2', display: 'inline-block', borderRadius: '3px'}} href="https://unsplash.com/@seefromthesky?utm_medium=referral&utm_campaign=photographer-credit&utm_content=creditBadge" target="_blank" rel="noopener noreferrer" title="Download free do whatever you want high-resolution photos from Ishan @seefromthesky"><span style={{display: 'inline-block', padding: '2px 3px'}}><svg xmlns="http://www.w3.org/2000/svg" style={{height: '12px', width: 'auto', position: 'relative', verticalAlign: 'middle', top: '-2px', fill: 'white'}} viewBox="0 0 32 32"><title>unsplash-logo</title><path d="M10 9V0h12v9H10zm12 5h10v18H0V14h10v9h12v-9z" /></svg></span><span style={{display: 'inline-block', padding: '2px 3px'}}>Ishan @seefromthesky</span></a>
+      </div>
+    );
+  }
+}
+
+
+class App extends React.Component{
+  state={
+    showSearch:true,
+  }
+  render(){
+    return (
+      <div className={Style.container}>
+      <img className={Style.background} src={require('./Images/background.jpg')} alt='animated Searchbar'/>
+      <div className={Style.container}>
+      <form className={Style.container} onSubmit={(e)=>{
+        e.preventDefault();
+        const serialize = function (form) {
+          // Setup our serialized data
+          var serialized = [];
+          // Loop through each field in the form
+          for (var i = 0; i < form.elements.length; i++) {
+            var field = form.elements[i];
+            // Don't serialize fields without a name, submits, buttons, file and reset inputs, and disabled fields
+            if (!field.name || field.disabled || field.type === 'file' || field.type === 'reset' || field.type === 'submit' || field.type === 'button') continue;
+            // If a multi-select, get all selections
+            if (field.type === 'select-multiple') {
+              for (var n = 0; n < field.options.length; n++) {
+                if (!field.options[n].selected) continue;
+                serialized.push(encodeURIComponent(field.name) + "=" + encodeURIComponent(field.options[n].value));
+              }
+            }
+            // Convert field data to a query string
+            else if ((field.type !== 'checkbox' && field.type !== 'radio') || field.checked) {
+              serialized.push(encodeURIComponent(field.name) + "=" + encodeURIComponent(field.value));
+            }
+          }
+          return serialized.join('&');
+        };
+        var form = e.target;
+        var formData = serialize(form);
+        var togo = formData.split('=')
+        window.location.href = `https://www.google.com/maps/place/${togo[1]}`
+      }}>
+
+      <Searchbar
+      name={'WhereToGo'}
+      className={Style.input}
+      ONLYSHOWONFOCUS={true}
+      blinker={true}
+      minLength={1}
+      maxLength={10}
+      typeList={worklist}
+      typeDelay={3000}
+      typeSpeed={{min:50, max:200}}
+
+      onClick={(e)=>{
+        console.log('onClick');
+      }}
+      onHover={(e)=>{
+        console.log('onHover');
+      }}
+      onFocus={(e)=>{
+        this.setState({
+          showSearch:false
+        })
+      }}
+      onBlur={(e)=>{
+        this.setState({
+          showSearch:true
+        })
+      }}
+      />
+      {
+      this.state.showSearch === true &&
+        <div className={Style.floater}>
+          <img className={Style.searchIcon} src={require('./Images/searchicon.png')} alt='animated Searchbar'/>
+        </div>
+      }
+      </form>
+      </div>
+      <NewComponent/>
+      </div>
+    );
+  }
 }
 
 export default App;
+//    <NewComponent/>
---
+++ src/input-text.js
@@ -0,0 +1,121 @@
+import React from 'react';
+import Style from './input-text.module.css';
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+}
+
+class App extends React.Component{
+  state={
+    placeholder : '',
+  }
+  typeList_int = 0;
+  typeDelay = this.props.typeDelay || 2500;
+  componentDidMount(){
+    this._blinker();
+    this._animatePlaceholder();
+    this._updatePlaceholder();
+  }
+  typespeed(){
+    const range = {
+      min : this.props.typeSpeed.min || 0,
+      max : this.props.typeSpeed.max || 1000,
+    }
+    var speed = getRandomInt(range.min, range.max);
+    return(speed)
+  }
+  _blinker(){
+    if(!this.props.blinker){return}
+    setInterval(()=>{
+      this.blinker = !this.blinker;
+    },500)
+  }
+  _animatePlaceholder(){
+    // Check if animatePlaceholder is null?
+    if(!this.animatePlaceholder){this.animatePlaceholder = ''}
+    setTimeout(()=>{
+      try{
+        var currentPlaceholder = this.props.typeList[this.typeList_int];
+        // Check if placeholder is fully typespeed
+        if(this.animatePlaceholder == currentPlaceholder){
+          setTimeout(()=>{
+            this.animatePlaceholder = '';
+            this.typeList_int += 1;
+            if(this.typeList_int >= this.props.typeList.length){
+              this.typeList_int = 0;
+            }
+            this._animatePlaceholder();
+          },this.typeDelay)
+        }else{
+          // Type our latters of typeList
+          var newPlaceholder = '';
+          for(var i = 0; i <= this.animatePlaceholder.length; i++){
+            newPlaceholder += currentPlaceholder.charAt(i);
+          }
+          this.animatePlaceholder = newPlaceholder;
+          this.setState({
+            animateDelay:this.typespeed(),
+          })
+          this._animatePlaceholder();
+        }
+      }catch(e){
+        console.log(e)
+      }
+    },this.state.animateDelay)
+  }
+  _updatePlaceholder(){
+    setInterval(()=>{
+      if(this.element === document.activeElement || !this.props.ONLYSHOWONFOCUS){
+        var placeholder = this.animatePlaceholder;
+        if(this.blinker){
+          placeholder += '_';
+        }
+        this.setState({
+          placeholder:placeholder,
+        })
+      }else{
+        this.setState({
+          placeholder:'',
+        })
+      }
+    },100)
+  }
+  render(){
+    return (
+      <input
+        ref={(r)=>{this.element = r}}
+        className={this.props.className || Style.default}
+        type="text"
+        name={this.props.name || 'AnimatedSearchbar'}
+        autoComplete={this.props.autocomplete || 'AnimatedSearchbar'}
+        list={this.props.list || null}//	The id of the <datalist> element that contains the optional pre-defined autocomplete options
+        maxLength={this.props.maxLength || 99}//	The maximum number of characters the input should accept
+        minLength={this.props.minLength || 0}//	The minimum number of characters long the input can be and still be considered valid
+        pattern={this.props.pattern || null}//	A regular expression the input's contents must match in order to be valid
+        placeholder={this.state.placeholder || null}//	An exemplar value to display in the input field whenever it is empty
+        readOnly={this.props.readonly || false}//	A Boolean attribute indicating whether or not the contents of the input should be read-only
+        size={this.props.size || null}//	A number indicating how many characters wide the input field should be
+        spellCheck={this.props.spellcheck || null}//	Controls whether or not to enable spell checking for the input field, or if the default spell checking configuration should be used
+
+        onClick={(e)=>{
+          try{
+            this.props.onClick(e);
+          }catch(e){}
+        }}
+        onFocus={(e)=>{
+          try{
+            this.props.onFocus(e);
+          }catch(e){}
+        }}
+        onBlur={(e)=>{
+          try{
+            this.props.onBlur(e);
+          }catch(e){}
+        }}
+      />
+    );
+  }
+}
+export default App;
